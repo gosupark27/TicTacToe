94fff0aba04dfe384dad4520fe28312d3894df8c
Heui Park <hpark41@wgu.edu>
2021-04-29 11:56:30 -0700

    test: create tests for ErrorHandling.cs and Player.cs

---
+++ TicTacToe.Tests/ErrorHandlingTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Tic_Tac_Toe_proto;
+
+namespace TicTacToe.tests
+{
+	public class ErrorHandlingTest
+	{
+		[Theory]
+		[InlineData('1',true)]
+		[InlineData('z', false)]
+		[InlineData('@', false)]
+		public void IsValidNumber_CheckBool(char testValue ,bool expected)
+		{
+			// Arrange
+			ErrorHandling eHandler = new ErrorHandling();
+			// Act
+			var actual = eHandler.IsValidNumber(testValue);
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+	}
+}
